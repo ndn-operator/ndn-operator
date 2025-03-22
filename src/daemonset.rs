4369eb6f57c1a9e2adff5bab720f4a9cc7069e17
Gitopolis <qnicks@gmail.com>
2025-03-22 15:10:39 -0700

    Mount /etc/ndnd folder to containers

--- src/daemonset.rs
+++ src/daemonset.rs
@@ -30,7 +30,7 @@ pub fn create_owned_daemonset(source: &Network) -> DaemonSet {
                     init_containers: Some(vec![Container {
                         name: "gencfg".to_string(),
                         image: Some("ghcr.io/gitopolis/ndn-operator:dev".to_string()),
-                        command: vec!["/genconfig".to_string(), "--output".to_string(), "/etc/example.yml".to_string()].into(),
+                        command: vec!["/genconfig".to_string(), "--output".to_string(), "/etc/ndnd/example.yml".to_string()].into(),
                         security_context: Some(SecurityContext {
                             privileged: Some(true),
                             ..SecurityContext::default()
@@ -38,7 +38,7 @@ pub fn create_owned_daemonset(source: &Network) -> DaemonSet {
                         volume_mounts: Some(vec![
                             VolumeMount {
                                 name: "config".to_string(),
-                                mount_path: "/etc/example.yml".to_string(),
+                                mount_path: "/etc/ndnd".to_string(),
                                 read_only: Some(false),
                                 ..VolumeMount::default()
                             },
@@ -49,7 +49,7 @@ pub fn create_owned_daemonset(source: &Network) -> DaemonSet {
                         name: "network".to_string(),
                         image: Some("ghcr.io/named-data/ndnd:latest".to_string()),
                         command: vec!["/ndnd".to_string()].into(),
-                        args: Some(vec!["daemon".to_string(), "/etc/example.yml".to_string()]),
+                        args: Some(vec!["daemon".to_string(), "/etc/ndnd/example.yml".to_string()]),
                         security_context: Some(SecurityContext {
                             privileged: Some(true),
                             ..SecurityContext::default()
@@ -57,7 +57,7 @@ pub fn create_owned_daemonset(source: &Network) -> DaemonSet {
                         volume_mounts: Some(vec![
                             VolumeMount {
                                 name: "config".to_string(),
-                                mount_path: "/etc/example.yml".to_string(),
+                                mount_path: "/etc/ndnd".to_string(),
                                 read_only: Some(true),
                                 ..VolumeMount::default()
                             },
@@ -73,8 +73,8 @@ pub fn create_owned_daemonset(source: &Network) -> DaemonSet {
                         Volume {
                             name: "config".to_string(),
                             host_path: Some(HostPathVolumeSource {
-                                path: "/etc/ndnd/example.yml".to_string(),
-                                type_: Some("FileOrCreate".to_string()),
+                                path: "/etc/ndnd".to_string(),
+                                type_: Some("DirectoryOrCreate".to_string()),
                                 ..HostPathVolumeSource::default()
                             }),
                             ..Volume::default()
