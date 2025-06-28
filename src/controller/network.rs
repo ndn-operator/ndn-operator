2a3e47c4f00adc262a92ef7602a893c0e8931f9d
Nikita Kutselev <132783525+Gitopolis@users.noreply.github.com>
2025-06-28 14:36:15 -0700

    Fix permissions when working in non-default namespaces (#9)
    * Add ndnd.image field to Network
    
    * Create daemonset in the network's namespace
    
    * Update dependencies
    
    * Lint
    
    ---------
    
    Co-authored-by: Gitopolis <qnicks@gmail.com>

--- src/controller/network.rs
+++ src/controller/network.rs
@@ -4,9 +4,8 @@ use k8s_openapi::{
     api::{
         apps::v1::{DaemonSet, DaemonSetSpec},
         core::v1::{
-            Container, ContainerPort, EnvVar, EnvVarSource, HostPathVolumeSource, ObjectFieldSelector, PodSpec,
-            PodTemplateSpec, SecurityContext, Volume, VolumeMount,
-        },
+            Container, ContainerPort, EnvVar, EnvVarSource, HostPathVolumeSource, ObjectFieldSelector, PodSpec, PodTemplateSpec, SecurityContext, ServiceAccount, Volume, VolumeMount
+        }, rbac::v1::{PolicyRule, Role, RoleBinding, RoleRef, Subject},
     },
     apimachinery::pkg::apis::meta::v1::{LabelSelector, ObjectMeta},
 };
@@ -21,6 +20,7 @@ use kube::{
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
+use serde_with::skip_serializing_none;
 use std::{collections::BTreeMap, sync::Arc};
 
 pub static NETWORK_FINALIZER: &str = "network.named-data.net/finalizer";
@@ -32,14 +32,30 @@ pub static CONTAINER_SOCKET_DIR: &str = "/run/ndnd";
 pub static HOST_CONFIG_DIR: &str = "/etc/ndnd";
 pub static HOST_SOCKET_DIR: &str = "/run/ndnd";
 
-#[derive(CustomResource, Deserialize, Serialize, Clone, Debug, JsonSchema)]
+#[derive(CustomResource, Deserialize, Serialize, Clone, Debug, Default, JsonSchema)]
 #[serde(rename_all = "camelCase")]
-#[kube(group = "named-data.net", version = "v1alpha1", kind = "Network", namespaced, shortname = "nw")]
+#[kube(group = "named-data.net", version = "v1alpha1", kind = "Network", derive="Default", namespaced, shortname = "nw")]
 #[kube(status = "NetworkStatus")]
 pub struct NetworkSpec {
     pub prefix: String,
     pub udp_unicast_port: i32,
     pub node_selector: Option<BTreeMap<String, String>>,
+    pub ndnd: Option<Ndnd>,
+}
+
+#[skip_serializing_none]
+#[derive(Deserialize, Serialize, Clone, Debug, JsonSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct Ndnd {
+    pub image: String,
+}
+
+impl Default for Ndnd {
+    fn default() -> Self {
+        Self {
+            image: "ghcr.io/named-data/ndnd:latest".to_string(),
+        }
+    }
 }
 
 #[derive(Deserialize, Serialize, Clone, Debug, JsonSchema)]
@@ -59,9 +75,20 @@ impl Network {
             .spec
             .expect("Failed to get pod spec");
         let my_image = my_pod_spec.containers.first().expect("Failed to get my container").image.clone();
-        let my_ns = my_pod.namespace().expect("Failed to get pod namespace");
-        let api_ds: Api<DaemonSet> = Api::namespaced(ctx.client.clone(), &my_ns);
-        let ds_data = self.create_owned_daemonset(my_image, my_pod_spec.service_account_name);
+        let ns = self.namespace().unwrap();
+        let api_sa: Api<ServiceAccount> = Api::namespaced(ctx.client.clone(), &ns);
+        let api_role: Api<Role> = Api::namespaced(ctx.client.clone(), &ns);
+        let api_role_binding: Api<RoleBinding> = Api::namespaced(ctx.client.clone(), &ns);
+        let api_ds: Api<DaemonSet> = Api::namespaced(ctx.client.clone(), &ns);
+        let sa_data = self.create_owned_sa();
+        let role_date = self.create_owned_role();
+        let role_binding_data = self.create_owned_role_binding(sa_data.name_any(), role_date.name_any());
+        let ds_data = self.create_owned_daemonset(my_image, Some(sa_data.name_any()));
+        // Create ServiceAccount
+        let _sa = api_sa.patch(&self.name_any(), &serverside, &Patch::Apply(sa_data)).await.map_err(Error::KubeError)?;
+        let _role = api_role.patch(&self.name_any(), &serverside, &Patch::Apply(role_date)).await.map_err(Error::KubeError)?;
+        let _role_binding = api_role_binding.patch(&self.name_any(), &serverside, &Patch::Apply(role_binding_data)).await.map_err(Error::KubeError)?;
+        // Create DaemonSet
         let ds = api_ds.patch(&self.name_any(), &serverside, &Patch::Apply(ds_data)).await.map_err(Error::KubeError)?;
         // Publish event
         ctx.recorder
@@ -132,7 +159,63 @@ impl Network {
         format!("{}/{}", HOST_CONFIG_DIR, self.config_file_name())
     }
 
-    pub fn create_owned_daemonset(&self, image: Option<String>, service_account: Option<String>) -> DaemonSet {
+    fn create_owned_sa(&self) -> ServiceAccount {
+        let oref = self.controller_owner_ref(&()).unwrap();
+        ServiceAccount {
+            metadata: ObjectMeta {
+                name: Some(self.name_any()),
+                owner_references: Some(vec![oref]),
+                ..ObjectMeta::default()
+            },
+            automount_service_account_token: Some(true),
+            ..ServiceAccount::default()
+        }
+    }
+
+    fn create_owned_role(&self) -> Role {
+        let oref = self.controller_owner_ref(&()).unwrap();
+        Role {
+            metadata: ObjectMeta {
+                name: Some(self.name_any()),
+                owner_references: Some(vec![oref]),
+                ..ObjectMeta::default()
+            },
+            rules: Some(vec![
+                PolicyRule {
+                    api_groups: Some(vec!["named-data.net".to_string()]),
+                    resources: Some(vec!["routers".to_string(), "routers/status".to_string()]),
+                    verbs: vec!["*".to_string()],
+                    ..PolicyRule::default()
+                },
+            ])
+        }
+    }
+
+    fn create_owned_role_binding(&self, sa_name: String, role_name: String) -> RoleBinding {
+        let oref = self.controller_owner_ref(&()).unwrap();
+        RoleBinding {
+            metadata: ObjectMeta {
+                name: Some(self.name_any()),
+                owner_references: Some(vec![oref]),
+                ..ObjectMeta::default()
+            },
+            role_ref: RoleRef {
+                api_group: "rbac.authorization.k8s.io".to_string(),
+                kind: "Role".to_string(),
+                name: role_name,
+            },
+            subjects: Some(vec![
+                Subject {
+                    kind: "ServiceAccount".to_string(),
+                    name: sa_name,
+                    namespace: Some(self.namespace().unwrap()),
+                    ..Subject::default()
+                },
+            ])
+        }
+    }
+
+    fn create_owned_daemonset(&self, image: Option<String>, service_account: Option<String>) -> DaemonSet {
         let oref = self.controller_owner_ref(&()).unwrap();
         let mut labels = BTreeMap::new();
         labels.insert(DS_LABEL_KEY.to_string(), self.name_any());
@@ -236,7 +319,7 @@ impl Network {
                         }]),
                         containers: vec![Container {
                             name: "network".to_string(),
-                            image: Some("ghcr.io/named-data/ndnd:20250405".to_string()),
+                            image: Some(self.spec.ndnd.clone().unwrap_or_default().image),
                             command: vec!["/ndnd".to_string()].into(),
                             args: Some(vec!["daemon".to_string(), container_config_path.to_string()]),
                             security_context: Some(SecurityContext {
@@ -386,11 +469,7 @@ impl TryFrom<OwnerReference> for Network {
                 name: Some(name),
                 ..Default::default()
             },
-            spec: NetworkSpec {
-                prefix: String::new(),
-                udp_unicast_port: 0,
-                node_selector: None,
-            },
+            spec: NetworkSpec::default(),
             status: None,
         })
     }
